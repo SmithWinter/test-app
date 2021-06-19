c425bd8de93cf8b95109d90edcaa521ae911fc44
Ho Vu Minh Duc <hovuminhduc@gmail.com>
2021-06-19 12:44:14 +0700

    first commit

---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+
+export const JWT_SECRET = 'JWTSECRET'
+export const PORT = process.env.PORT || 5000
+export const MONGO_URI = 'mongodb+srv://root:admin@cluster0.vounz.mongodb.net/cmscart?retryWrites=true&w=majority'
+
